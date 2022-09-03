ae0a928de912539acd597b997805215c21a7f580
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-09-04 00:30:52 +0600

    modal details completed

--- js/app.js
+++ js/app.js
@@ -8,6 +8,91 @@ const toggleSpinner = (toggle) => {
   }
 };
 
+// load default news category
+const loadDefaultNews = (id) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayDefaultNews(data.data))
+    .catch((error) => console.log(error));
+};
+
+const displayDefaultNews = (allNews) => {
+  const itemsCounter = document.getElementById("items-counter");
+  itemsCounter.innerText = `${allNews.length} items found in All News category`;
+
+  const newsContainer = document.getElementById("news-container");
+  newsContainer.innerHTML = ``;
+  allNews.forEach((singleNews) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card", "mb-3");
+    cardDiv.innerHTML = `
+    <div class="row g-0 p-3 bg-light">
+      <div class="col-md-4">
+        <img src="${
+          singleNews?.image_url
+        }" class="img-fluid rounded-start" alt="" />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body ms-4">
+          <h5 class="card-title fs-4">
+            ${singleNews?.title}
+          </h5>
+          <p class="card-text text-muted">
+            ${singleNews?.details.slice(0, 400) + "..."}
+          </p>
+          <div
+            class="d-flex justify-content-between align-items-center"
+          >
+            <div class="d-flex">
+              <img
+                class="author-img img-fluid rounded-circle profile-img ms-0 me-2 h-100"
+                src="${singleNews?.author.img}"
+                alt=""
+              />
+              <div class="d-flex flex-column justify-content-center">
+                <span><b>${
+                  singleNews.author.name
+                    ? singleNews.author.name
+                    : "No author found"
+                }</b></span>
+                <span class="text-muted">${
+                  singleNews.author?.published_date
+                    ? singleNews.author?.published_date
+                    : "No date available"
+                }</span>
+              </div>
+            </div>
+            <div>
+              <i class="fa-solid fa-eye me-1"></i>
+              <span>${
+                singleNews.total_view ? singleNews.total_view : "No data found"
+              }</span>
+            </div>
+            <div>
+              <i class="fa-regular fa-star-half-stroke"></i>
+              <i class="fa-regular fa-star"></i>
+              <i class="fa-regular fa-star"></i>
+              <i class="fa-regular fa-star"></i>
+              <i class="fa-regular fa-star"></i>
+            </div>
+            <div>
+              <button onclick=showDetails('${
+                singleNews._id
+              }') class="btn-news-details bg-light" data-bs-toggle="modal"
+              data-bs-target="#newsDetailsModal">
+                <i class="fa-solid fa-arrow-right"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+    `;
+    newsContainer.appendChild(cardDiv);
+  });
+};
+
 // list of categories in the header section
 const loadCategories = () => {
   fetch("https://openapi.programming-hero.com/api/news/categories")
@@ -70,7 +155,7 @@ const displayNewsInCards = (allNews, text) => {
           >
             <div class="d-flex">
               <img
-                class="author-img img-fluid rounded-circle profile-img ms-0 me-2"
+                class="author-img img-fluid rounded-circle profile-img ms-0 me-2 h-100"
                 src="${singleNews?.author.img}"
                 alt=""
               />
@@ -82,6 +167,8 @@ const displayNewsInCards = (allNews, text) => {
                 }</b></span>
                 <span class="text-muted">${
                   singleNews.author?.published_date
+                    ? singleNews.author?.published_date
+                    : "No date available"
                 }</span>
               </div>
             </div>
@@ -99,7 +186,10 @@ const displayNewsInCards = (allNews, text) => {
               <i class="fa-regular fa-star"></i>
             </div>
             <div>
-              <button class="btn-news-details bg-light">
+              <button onclick=showDetails('${
+                singleNews._id
+              }') class="btn-news-details bg-light" data-bs-toggle="modal"
+              data-bs-target="#newsDetailsModal">
                 <i class="fa-solid fa-arrow-right"></i>
               </button>
             </div>
@@ -113,5 +203,30 @@ const displayNewsInCards = (allNews, text) => {
   toggleSpinner(false);
 };
 
+// display details of news in modal
+const showDetails = (news_id) => {
+  const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayModalDetails(data.data[0]))
+    .catch((error) => console.log(error));
+};
+
+const displayModalDetails = (singleNewsDetails) => {
+  const modalLabelTitle = document.getElementById("modal-label-title");
+  const newsDetailsImg = document.getElementById("news-details-img");
+  const newsDetailsDesc = document.getElementById("news-details-desc");
+  const newsDetailsAuthor = document.getElementById("news-details-author");
+  const newsDetailsDate = document.getElementById("news-details-date");
+
+  modalLabelTitle.innerText = singleNewsDetails.title;
+  newsDetailsImg.setAttribute("src", `${singleNewsDetails.image_url}`);
+  newsDetailsDesc.innerText = singleNewsDetails.details;
+  newsDetailsAuthor.innerText = singleNewsDetails.author.name
+    ? singleNewsDetails.author.name
+    : "Unknown";
+  newsDetailsDate.innerText = singleNewsDetails.author.published_date;
+};
+
 loadCategories();
-// loadNewsInCards("01");
+loadDefaultNews("08");
