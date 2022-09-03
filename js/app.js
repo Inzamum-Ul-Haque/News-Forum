b927aaadcdffb4a55ebb7e0b82441b241117540f
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-09-03 20:38:53 +0600

    spinner added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+// toggle spinner
+const toggleSpinner = (toggle) => {
+  const spinnerDiv = document.getElementById("spinner-div");
+  if (toggle) {
+    spinnerDiv.classList.remove("d-none");
+  } else {
+    spinnerDiv.classList.add("d-none");
+  }
+};
+
 // list of categories in the header section
 const loadCategories = () => {
   fetch("https://openapi.programming-hero.com/api/news/categories")
@@ -10,30 +20,36 @@ const displayCategories = (news) => {
   const categoriesList = document.getElementById("categories-list");
   news.forEach((singleNews) => {
     const li = document.createElement("li");
-    li.setAttribute("onclick", `loadNewsInCards('${singleNews.category_id}')`);
+    li.setAttribute(
+      "onclick",
+      `loadNewsInCards('${singleNews.category_id}', event)`
+    );
     li.innerText = singleNews.category_name;
     categoriesList.appendChild(li);
   });
 };
 
-const loadNewsInCards = (id) => {
+// displaying news in cards section
+const loadNewsInCards = (id, event) => {
+  toggleSpinner(true);
   const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayNewsInCards(data.data))
+    .then((data) => displayNewsInCards(data.data, event.target.innerText))
     .catch((error) => console.log(error));
 };
 
-const displayNewsInCards = (allNews) => {
+const displayNewsInCards = (allNews, text) => {
+  //setting the items counter
+  const itemsCounter = document.getElementById("items-counter");
+  itemsCounter.innerText = `${allNews.length} items found in ${text} category`;
+
+  //setting the cards section
   const newsContainer = document.getElementById("news-container");
   newsContainer.innerHTML = ``;
   allNews.forEach((singleNews) => {
     const cardDiv = document.createElement("div");
     cardDiv.classList.add("card", "mb-3");
-
-    // slicing the texts
-    // const newsDescription = singleNews.details.slice(0, 400) + "...";
-    // console.log(newsDescription);
     cardDiv.innerHTML = `
     <div class="row g-0 p-3 bg-light">
       <div class="col-md-4">
@@ -94,6 +110,7 @@ const displayNewsInCards = (allNews) => {
     `;
     newsContainer.appendChild(cardDiv);
   });
+  toggleSpinner(false);
 };
 
 loadCategories();
